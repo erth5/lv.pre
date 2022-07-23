f64146424185983a2ce5616c8cf087e194643b8e
erth5 <Rthem@gmx.de>
2022-07-23 22:52:58 +0200

    understanding session1

--- resources/views/components/debug/sessionStatus.blade.php
+++ resources/views/components/debug/sessionStatus.blade.php
@@ -1,3 +1,4 @@
+{{-- use with "with" --}}
 <div style="text-align: center">
     @if (session('status'))
         <p> {{ session('status') }} </p>
@@ -8,18 +9,17 @@
     @if (session('error'))
         <p> {{ session('error') }} </p>
     @endif
-</div>
 
-@if (session('statusInfo'))
-    <p> {{ session('statusInfo') }} </p>
-@endif
-@if (session('statusSuccess'))
-    <p> {{ session('statusSuccess') }} </p>
-@endif
-{{-- official, layout-compatible --}}
-@if (session('statusError'))
-    <p> {{ session('statusError') }} </p>
-@endif
+    @if (session('statusInfo'))
+        <p> {{ session('statusInfo') }} </p>
+    @endif
+    @if (session('statusSuccess'))
+        <p> {{ session('statusSuccess') }} </p>
+    @endif
+    @if (session('statusError'))
+        <p> {{ session('statusError') }} </p>
+    @endif
+</div>
 
 @if ($errors->any())
     {{ $errors->first() }}
