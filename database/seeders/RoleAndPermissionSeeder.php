4f5b20c70f7ef7c38514719cd9779587efee3228
ruben.matthes <rthem@gmx.de>
2022-07-25 19:01:47 +0200

    update

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -3,11 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use GuzzleHttp\Promise\Create;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RoleAndPermissionSeeder extends Seeder
 {
